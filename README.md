ec407f4d3312c5c99cc9ff05fade8faaf92926f0
pagxir <pagxir@gmail.com>
2013-01-30 00:14:55 +0800

    update build script

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ android-develop-script
 ======================
 
 some script for android develop
+
+su.zip: su update package for C8650
--- signature-package.sh
+++
@@ -1,1 +0,0 @@
-java  –jar  myandroid/out/host/linux-x86/framework/signapk.jar  –w  myandroid/build/target/product/security/testkey.x509.pem  /testkey.pk8  update.zip  update_signed.zip
