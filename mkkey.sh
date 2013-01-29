04ab84f8d21276bd2524122f5b802fc295907a25
pagxir <pagxir@gmail.com>
2013-01-29 23:42:59 +0800

    add signapk tool script

---
+++ mkkey.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+function make_key
+{
+	if [ "$1" == "" ];
+	then
+		echo "Create a test certificate key."
+		echo "Usage: $0 NAME"
+		echo "Will generate NAME.pk8 and NAME.x509.pem"
+		echo "  /C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com"
+		return;
+	fi
+
+		openssl genrsa -3 -out $1.pem 2048
+		openssl req -new -x509 -key testkey.pem -out testkey.x509.pem -days 10000 -subj "/C=CN/ST=Shang Hai/L=Pu Dong/O=Ovenstone/OU=Ovenstone/CN=Ovenstone/emailAddress=pagxir@gmail.com"
+		openssl pkcs8 -in $1.pem -topk8 -outform DER -out $1.pk8 -nocrypt
+}
+
+make_key testkey
+
---
+++ signapk.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+SIGNED_NAME=$(echo $1|sed 's/\.\([a-z0-9]*\)/_signed.\1/')
+java -jar signapk.jar testkey.x509.pem testkey.pk8 ${1} ${SIGNED_NAME}
+zipalign -v 4 ${SIGNED_NAME} ${1}
+rm ${SIGNED_NAME}
