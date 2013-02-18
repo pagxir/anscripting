838e96d18976ddb6bacbfcda508f5bc24223f252
pagxir <pagxir@gmail.com>
2013-02-18 21:48:22 +0800

    add zipalign support

--- signapk.sh
+++ signapk.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 SIGNED_NAME=$(echo $1|sed 's/\.\([a-z0-9]*\)/_signed.\1/')
+ALIGNED_NAME=$(echo $1|sed 's/\.\([a-z0-9]*\)/_aligned.\1/')
 java -jar signapk.jar -w testkey.x509.pem testkey.pk8 ${1} ${SIGNED_NAME}
-#zipalign -v 4 ${SIGNED_NAME} ${1}
-#rm ${SIGNED_NAME}
+export PATH=${PATH}:`pwd`
+zipalign -v 4 ${SIGNED_NAME} ${ALIGNED_NAME}
+rm ${SIGNED_NAME}
