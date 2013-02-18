90b4b067da522cc100c5f30dc5d913b8edc484d3
pagxir <pagxir@gmail.com>
2013-02-18 20:41:29 +0800

    update mkkey script

--- mkkey.sh
+++ mkkey.sh
@@ -11,8 +11,8 @@ function make_key
 		return;
 	fi
 
-		openssl genrsa -3 -out $1.pem 2048
-		openssl req -new -x509 -key testkey.pem -out testkey.x509.pem -days 10000 -subj "/C=CN/ST=Shang Hai/L=Pu Dong/O=Ovenstone/OU=Ovenstone/CN=Ovenstone/emailAddress=pagxir@gmail.com"
+#		openssl genrsa -3 -out $1.pem 2048
+		openssl req -new -x509 -key $1.pem -out $1.x509.pem -days 10000 -subj "/C=CN/ST=Shang Hai/L=Pu Dong/O=Ovenstone/OU=Ovenstone/CN=Ovenstone/emailAddress=pagxir@gmail.com"
 		openssl pkcs8 -in $1.pem -topk8 -outform DER -out $1.pk8 -nocrypt
 }
 
