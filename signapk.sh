bbbe3eee1a51971cdc8f229940092920a73d2e61
pagxir <pagxir@gmail.com>
2013-07-03 23:14:51 +0800

    add testkey to keep signature ok for all my apk

--- signapk.sh
+++ signapk.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-SIGNED_NAME=$(echo $1|sed 's/\.\([a-z0-9]*\)/_signed.\1/')
-ALIGNED_NAME=$(echo $1|sed 's/\.\([a-z0-9]*\)/_aligned.\1/')
-java -jar signapk.jar -w testkey.x509.pem testkey.pk8 ${1} ${SIGNED_NAME}
+SIGNED_NAME=$(echo $1|sed 's/\.\([a-z0-9]*\)$/_signed.\1/')
+ALIGNED_NAME=$(echo $1|sed 's/\.\([a-z0-9]*\)$/_aligned.\1/')
+java -jar signapk.jar testkey.x509.pem testkey.pk8 ${1} ${SIGNED_NAME}
 export PATH=${PATH}:`pwd`
 zipalign -v 4 ${SIGNED_NAME} ${ALIGNED_NAME}
 rm ${SIGNED_NAME}
